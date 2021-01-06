5162201871f9867110627325d692a8cdcaae95c9
Wilson <karaewilson16@gmail.com>
2021-01-06 20:31:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,9 +30,5 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch (age) {
-  case '13', '14', '15', '16', '17', '18', '19':
-    return "You are a teenager";
-    default:
-    return "You have an age";
+
 }
