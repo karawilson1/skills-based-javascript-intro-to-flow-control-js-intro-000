ddc1ec6ef14960334bde37c2992789b5a94b469c
Wilson <karaewilson16@gmail.com>
2021-01-06 20:20:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,7 +32,7 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch (age) {
   case '13', '14', '15', '16', '17', '18', '19':
-    console.log("You are a teenager");
+    return("You are a teenager");
     default:
-    console.log("You have an age");
+    return("You have an age");
 }
